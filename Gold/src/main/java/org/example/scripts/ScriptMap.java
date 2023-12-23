a512ed98b145372b4430d4413c08e4fb8611145f
Daniil-Tsiunchyk <fcad.td@gmail.com>
2023-12-23 17:56:08 +0300

    feat: ended with map

--- Gold/src/main/java/org/example/scripts/ScriptMap.java
+++ Gold/src/main/java/org/example/scripts/ScriptMap.java
@@ -44,15 +44,17 @@ public class ScriptMap {
 
     private static class MapDrawer extends JPanel {
         private final BattleMap battleMap;
+        private final int gridSize = 25;
 
         public MapDrawer(BattleMap battleMap) {
             this.battleMap = battleMap;
-            setPreferredSize(new Dimension(2000, 2000));
+            setPreferredSize(new Dimension(1000, 1000));
         }
 
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
+            drawGrid(g);
             if (battleMap != null) {
                 for (Island island : battleMap.getIslands()) {
                     drawIsland(g, island);
@@ -63,15 +65,25 @@ public class ScriptMap {
         private void drawIsland(Graphics g, Island island) {
             int[][] map = island.getMap();
             List<Integer> start = island.getStart();
-            int startX = start.get(0);
-            int startY = start.get(1);
+            int startX = start.get(0) / 2;
+            int startY = start.get(1) / 2;
 
             for (int i = 0; i < map.length; i++) {
                 for (int j = 0; j < map[i].length; j++) {
                     if (map[i][j] == 1) {
-                        g.fillRect(startX + j * 1, startY + i * 1, 1, 1);
+                        g.fillRect(startX + j / 2, startY + i / 2, 1, 1);
                     }
                 }
+            }
+        }
+
+        private void drawGrid(Graphics g) {
+            g.setColor(Color.GRAY);
+            for (int i = 0; i <= getWidth(); i += gridSize) {
+                g.drawLine(i, 0, i, getHeight());
+            }
+            for (int i = 0; i <= getHeight(); i += gridSize) {
+                g.drawLine(0, i, getWidth(), i);
             }
         }
     }
