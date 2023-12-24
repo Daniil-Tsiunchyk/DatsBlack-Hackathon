59e61cd50d0c6357a86e21e1304b0526bdab469a
Daniil-Tsiunchyk <fcad.td@gmail.com>
2023-12-24 10:15:32 +0300

    Updated map

--- Gold/src/main/java/org/example/Const.java
+++ Gold/src/main/java/org/example/Const.java
@@ -9,7 +9,7 @@ public class Const {
     public static final String apiKey = "a7e670d0-c3ff-4467-96ca-3ffcd79dca89";
     public static final Gson gson = new Gson();
     public static final String baseUrl = "https://datsblack.datsteam.dev/api/";
-    public static final String mapUrl = "https://datsblack.datsteam.dev/json/map/6586ae233e7ba8.12192069.json";
+    public static final String mapUrl = "https://datsblack.datsteam.dev/json/map/6586b281c9dc77.18944787.json";
     public final static int DISTANCE_SCAN = 16;
     public static final String NORTH = "north";
     public static final String SOUTH = "south";
